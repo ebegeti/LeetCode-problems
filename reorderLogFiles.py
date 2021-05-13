9464ff53133bc7f8e43c216261001c62daadb2ec
ebegeti <84118297+ebegeti@users.noreply.github.com>
2021-05-13 11:45:27 +0300

    Update reorderLogFiles.py

--- reorderLogFiles.py
+++ reorderLogFiles.py
@@ -1,3 +1,18 @@
+'''
+You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
+
+There are two types of logs:
+
+Letter-logs: All words (except the identifier) consist of lowercase English letters.
+Digit-logs: All words (except the identifier) consist of digits.
+Reorder these logs so that:
+
+The letter-logs come before all digit-logs.
+The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
+The digit-logs maintain their relative ordering.
+Return the final order of the logs.
+'''
+
 class Solution(object):
     def reorderLogFiles( logs):
         """
@@ -20,20 +35,7 @@ class Solution(object):
         print([log[0] for log in letter_logs] + digit_logs)
         return ([log[0] for log in letter_logs] + digit_logs)
 
-        # identifiers = []
-        # letter_logs = []
-        # digit_logs = []
-        # for log in logs:
-        #     new_list = log.split(' ')
-        #     if new_list[1].isdigit():
-        #         digit_logs.append(new_list[1:])
-        #     else:
-        #         letter_logs.append(new_list[1:])
-        #     identifiers.append(new_list[0][:-1])
-        # letter_logs.sort()
-        # digit_logs.sort()
-        #
-        # print(letter_logs + digit_logs)
+ 
 if __name__ == '__main__':
     sol=Solution.reorderLogFiles(logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"])
     if sol==["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]:
