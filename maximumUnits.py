35eb2426a714531e8d0a55b27cc79294ae005b62
ebegeti <84118297+ebegeti@users.noreply.github.com>
2021-05-13 11:35:28 +0300

    Update maximumUnits.py

--- maximumUnits.py
+++ maximumUnits.py
@@ -1,3 +1,13 @@
+'''
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+'''
+
 class Solution(object):
     def maximumUnits( boxTypes, truckSize):
         """
@@ -21,6 +31,7 @@ class Solution(object):
         if remained!=0:
             max_units+=remained*boxTypes[i][1]
         return max_units
+    
 if __name__ == '__main__':
     #[[1,3],[2,2],[3,1]]
     #Solution.maximumUnits([[1,3],[2,2],[3,1]],4)
