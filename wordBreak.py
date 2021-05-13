502010034896e5534b331bcc7b3f1e93d0466718
ebegeti <84118297+ebegeti@users.noreply.github.com>
2021-05-13 11:50:07 +0300

    Update wordBreak.py

--- wordBreak.py
+++ wordBreak.py
@@ -1,17 +1,15 @@
+'''
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+'''
+
 def wordBreak( s, wordDict):
     """
     :type s: str
     :type wordDict: List[str]
     :rtype: bool
     """
-
-    ##it doesnt work for case: cars,[car,ca,rs]
-    # for each in wordDict:
-    #     if each in s:
-    #         s=s.replace(each,'')
-    #     if s=='':
-    #         return True
-    # return False
 
     starts = [0]
     for i in range(len(s)):
