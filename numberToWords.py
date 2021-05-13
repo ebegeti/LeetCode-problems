0eca24e2368e66f7022f2a06284d2afa7d2ec08f
ebegeti <84118297+ebegeti@users.noreply.github.com>
2021-05-13 11:40:13 +0300

    Update numberToWords.py

--- numberToWords.py
+++ numberToWords.py
@@ -1,3 +1,7 @@
+'''
+Convert a non-negative integer num to its English words representation.
+'''
+
 digit = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine',
           10: 'Ten'}
 teens ={10:'Ten',11:'Eleven',12:'Twelve',13:'Thirteen',14:'Fourteen',15:'Fifteen',16:'Sixteen',17:'Seventeen',18:'Eighteen',19:'Nineteen'}
